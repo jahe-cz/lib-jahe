9d9fd4cedd9a3ae787b1fb03337868672acfb9ac
Radek Pinc (pincr) <radek.pinc@softinel.cz>
2025-09-14 21:34:35 +0200

    EDU / Generic interface for DOBI beans.

--- src/main/java/cz/jahe/dobi/DobiBean.java
+++ src/main/java/cz/jahe/dobi/DobiBean.java
@@ -2,8 +2,8 @@
 package cz.jahe.dobi;
 
 /**
- * 
- * 
+ * Generic interface for DOBI beans.
+ *  
  * @author Radek Pinc (pincr) {@literal <radek.pinc@softinel.cz>}
  */
 public interface DobiBean {
